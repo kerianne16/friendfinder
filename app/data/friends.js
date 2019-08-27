144cfeb410228673d60431f46266f45fd3f306c0
kerianne16 <kerianne116@gmail.com>
2019-08-27 18:56:20 -0400

    images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         "name": "Michael",
-        "photo": "https://media.glamour.com/photos/5c449a70cd8ee42d78c8f7a6/16:9/w_1280%2Cc_limit/sarah-jessica-parker-carrie-bradshaw.jpg",
+        "photo": "./michael.jpeg",
         "scores": [
             1,
             1,
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         "name": "Jim",
-        "photo": "https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/48/hbz-samantha-jones-32-shutterstock.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        "photo": "./jim.jpg",
         "scores": [
             2,
             2,
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         "name": "Pam",
-        "photo": "http://www2.pictures.zimbio.com/mp/hvo0yvbkNLAl.jpg",
+        "photo": "./pam.jpeg",
         "scores": [
             3,
             3,
@@ -49,7 +49,7 @@ var friends = [
     },
     {
         "name": "Dwight",
-        "photo": "https://i.pinimg.com/originals/71/74/25/7174256e73a6ea07423518eb8b6de93f.jpg",
+        "photo": "./dwight.jpg",
         "scores": [
             4,
             4,
